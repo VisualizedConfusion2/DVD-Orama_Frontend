ddb8bcf1c291c6f9a177ff0a117e2d7ad159a59e
Bjarke Erik Hjalmar Kusk Arvedlund <bja001@edu.zealand.dk>
2026-05-17 15:34:18 +0200

    SearchFunctionality: Update search functionality and UI

--- index.js
+++ index.js
@@ -1,36 +1,52 @@
 const baseUri = "https://dvd-oramaservices-e5bfgqbse9g5edg7.swedencentral-01.azurewebsites.net/api/"
 console.log("Base URI:", baseUri) // <-- check the base URI
 Vue.createApp({
-    data(){
-        return{
+    data() {
+        return {
             movies: [],
             movie: null,
             UserName: null,
+            searchTitle: '',
+            isSearching: false,
         }
     },
-    async created() {console.log("Base URI in created hook:", baseUri+"movie") // <-- check the base URI in created hook
-        console.log("created method called")
-        this.getMovies(baseUri+"movie") // <-- check the URI being called
-
+    async created() {
+        this.getMovies(baseUri + "movie")
     },
     methods: {
-            getAllMovies(){
-                this.getMovies(baseUri+"movie") // <-- check the URI being called
-            },
-            async getMovies(Uri) {
-        try {
-            const response = await axios.get(Uri);
-
-            console.log("RAW RESPONSE:", response);
-            console.log("DATA:", response.data);
-
-            this.movies = response.data;
-            console.log("MOVIES STATE:", this.movies);
-
-        } catch (ex) {
-            console.log(this.baseUri)
-            console.log("ERROR:", ex);
+        getAllMovies() {
+            this.getMovies(baseUri + "movie")
+        },
+        async getMovies(Uri) {
+            try {
+                const response = await axios.get(Uri);
+                this.movies = response.data;
+            } catch (ex) {
+                console.log("ERROR:", ex);
+            }
+        },
+        async searchMovies() {
+            if (!this.searchTitle.trim()) {
+                this.clearSearch();
+                return;
+            }
+            try {
+                const response = await axios.get(baseUri + "movie/search", {
+                    params: { title: this.searchTitle }
+                });
+                this.movies = response.data;
+                this.isSearching = true;
+            } catch (ex) {
+                if (ex.response?.status === 404) {
+                    this.movies = [];
+                }
+                console.log("ERROR:", ex);
+            }
+        },
+        clearSearch() {
+            this.searchTitle = '';
+            this.isSearching = false;
+            this.getMovies(baseUri + "movie");
         }
-    },
     }
 }).mount("#app")

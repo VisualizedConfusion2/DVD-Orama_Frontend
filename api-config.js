54bd05195fec0a4fe4c19fa59d9cd7b6986cb148
PatrickJarshoej <paj003@edu.zealand.dk>
2026-05-21 14:05:28 +0200

    Delete method now works

--- api-config.js
+++ api-config.js
@@ -1,6 +1,6 @@
 // Copy this file to api-config.js and set the URL for your environment.
 // Local: "http://localhost:5000/api/"
 // Production: "https://dvd-oramaservices-e5bfgqbse9g5edg7.swedencentral-01.azurewebsites.net/api/"
-// export const BASE_URL = "http://localhost:5178/api/";
+export const BASE_URL = "http://localhost:5178/api/";
 //made it a new
-export const BASE_URL = "https://dvd-oramaservices-e5bfgqbse9g5edg7.swedencentral-01.azurewebsites.net/api/";
+// export const BASE_URL = "https://dvd-oramaservices-e5bfgqbse9g5edg7.swedencentral-01.azurewebsites.net/api/";

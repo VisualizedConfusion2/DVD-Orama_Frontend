4a6497824a1e990e1fc3850af65135b038d07fd0
Theodor Hennecke <thh005@edu.zealand.dk>
2026-05-21 12:31:34 +0200

    commit2inbranch

--- api-config.js
+++ api-config.js
@@ -3,4 +3,4 @@
 // Production: "https://dvd-oramaservices-e5bfgqbse9g5edg7.swedencentral-01.azurewebsites.net/api/"
 // export const BASE_URL = "http://localhost:5178/api/";
 //made it a new
-export const BASE_URL = "http://localhost:5178/api/";
+export const BASE_URL = "https://dvd-oramaservices-e5bfgqbse9g5edg7.swedencentral-01.azurewebsites.net/api/";
